e82caa6c58d034c0eea1246840a2a777ff8b2f80
Gabriel Mesquita <69008067+GabrielMesquita00@users.noreply.github.com>
2024-03-06 14:37:50 -0300

    Add files via upload

---
+++ exInversaoString/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase ou palavra: ");
+        string palavra = Console.ReadLine();
+
+        string palavraContraria = Inversao(palavra); // chama função de inversão de palavra
+
+        Console.WriteLine("\nPalavra normal: " + palavra);
+        Console.WriteLine("Palavra invertida: " + palavraContraria);
+
+        Console.ReadLine();
+
+    }
+
+    static string Inversao(string str)
+    {
+        string invertida = ""; // aramazenamento da palavra invertida
+
+        for (int i = str.Length - 1; i >= 0; i--) // percorre a string da última posição até a primeira
+        {
+            invertida = invertida + str[i]; // e adiciona cada caractere à string invertida 
+
+        }
+        return invertida;
+    }
+}
